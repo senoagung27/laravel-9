2705e96836f4ae7bca4649509f2750a156193fe0
Seno Agung Pradityo <senoagung27@gmail.com>
2023-06-26 18:25:28 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,12 @@
 namespace Database\Seeders;
 
 use Carbon\Carbon;
+use App\Models\User;
 use App\Models\StokJubelio;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
@@ -18,7 +20,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
         // StokJubelio::factory(20)->create();
         // insert data ke table pegawai
         // DB::table('stok_jubelios')->insert([
@@ -56,45 +58,45 @@ class DatabaseSeeder extends Seeder
         // ]);
 
 
-        DB::table('stok_f_f_s')->insert([
-        	'nama_tenant' => 'Dingin Segar',
-        	'sku_number' => 'INDM2235',
-        	'nama_barang' => 'Indomie Soto',
-        	'lokasi_gudang' => 'Jakarta Selatan',
-        	'total_stok' => 5
-        ]);
-        DB::table('stok_f_f_s')->insert([
-        	'nama_tenant' => 'Dingin Segar',
-        	'sku_number' => 'INDM2232',
-        	'nama_barang' => 'Indomie Ayam Bawang',
-        	'lokasi_gudang' => 'Jakarta Selatan',
-        	'total_stok' => 5
-        ]);
-        DB::table('stok_f_f_s')->insert([
-        	'nama_tenant' => 'Dingin Segar',
-        	'sku_number' => 'DK2234',
-        	'nama_barang' => 'Indomie Ayam Bawang',
-        	'lokasi_gudang' => 'Jakarta Selatan',
-        	'total_stok' => 3
-        ]);
+        // DB::table('stok_f_f_s')->insert([
+        // 	'nama_tenant' => 'Dingin Segar',
+        // 	'sku_number' => 'INDM2235',
+        // 	'nama_barang' => 'Indomie Soto',
+        // 	'lokasi_gudang' => 'Jakarta Selatan',
+        // 	'total_stok' => 5
+        // ]);
+        // DB::table('stok_f_f_s')->insert([
+        // 	'nama_tenant' => 'Dingin Segar',
+        // 	'sku_number' => 'INDM2232',
+        // 	'nama_barang' => 'Indomie Ayam Bawang',
+        // 	'lokasi_gudang' => 'Jakarta Selatan',
+        // 	'total_stok' => 5
+        // ]);
+        // DB::table('stok_f_f_s')->insert([
+        // 	'nama_tenant' => 'Dingin Segar',
+        // 	'sku_number' => 'DK2234',
+        // 	'nama_barang' => 'Indomie Ayam Bawang',
+        // 	'lokasi_gudang' => 'Jakarta Selatan',
+        // 	'total_stok' => 3
+        // ]);
 
-        DB::table('stok_f_f_s')->insert([
-        	'nama_tenant' => 'Dingin Segar',
-        	'sku_number' => 'DK2233',
-        	'nama_barang' => 'Indomie Kari Ayam',
-        	'lokasi_gudang' => 'Jakarta Selatan',
-        	'total_stok' => 5
-        ]);
+        // DB::table('stok_f_f_s')->insert([
+        // 	'nama_tenant' => 'Dingin Segar',
+        // 	'sku_number' => 'DK2233',
+        // 	'nama_barang' => 'Indomie Kari Ayam',
+        // 	'lokasi_gudang' => 'Jakarta Selatan',
+        // 	'total_stok' => 5
+        // ]);
         // StokJubelio::factory()
         //     ->count(50)
         //     ->hasPosts(1)
         //     ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => '12341234',
-        //     // 'remember_token' => Str::random(10)
-        // ]);
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12341234'),
+            'remember_token' => Str::random(10)
+        ]);
     }
 }
